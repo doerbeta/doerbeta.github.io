0723c7a5301d16e8cf4107186f3d36d887530c69
Baihan Lin <nature@gatech.edu>
2019-12-19 22:12:12 -0500

    [DBG] test scatterplotting

--- viz/jsfunctions/tda.js
+++ viz/jsfunctions/tda.js
@@ -13,3 +13,21 @@ function dummyGraph() {
             .height(600)
     });
 }
+
+function pcaTDAGraph() {
+    fetch('../datasets/farrell2018_pca.json').then(res => res.json()).then(data => {
+        const Graph = ForceGraph()
+            (document.getElementById('graph'))
+            .graphData(data)
+            .nodeId('id')
+            .nodeRelSize(4)
+            .nodeVal('val')
+            .nodeLabel('id' + ':' + 'group')
+            .nodeAutoColorBy('group')
+            .linkSource('source')
+            .linkTarget('target')
+            .height(600)
+    });
+}
+
+var nodes, links
