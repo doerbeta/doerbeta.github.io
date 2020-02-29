adadd525fe89ef4b15d70dbdf7e0bcf408403f2b
Baihan Lin <nature@gatech.edu>
2020-02-29 05:17:44 -0500

    [DBG] fix color typo

--- viz/VoiceID/main.js
+++ viz/VoiceID/main.js
@@ -470,18 +470,6 @@
       var neg_reward = 0;
       var clicked = false;
 
-      document.getElementById("voiceid").innerHTML = arms[action]+' is speaking...';
-      console.log(action);
-      for (var i = 0; i < clickers.length; i++) {
-        if (math.equal(i,action)) {
-          clickers[i].style.backgroundColor = "#4CAF50";
-          clickers[i].style.color =  "white";
-        } else {
-          clickers[i].style.backgroundColor = "#white";
-          clickers[i].style.color =  "4CAF50";
-        }
-      }
-
       document.getElementById("btnnew").onclick = function() {
         addBtn(nspeakers);
         arms.push('User '+nspeakers);
@@ -543,6 +531,18 @@
         var a_prime = nearestNeighbor(x);
         // console.log(a_prime);
         updateRewardMapping(a_prime,x,pos_reward);
+      }
+
+      document.getElementById("voiceid").innerHTML = arms[action]+' is speaking...';
+      console.log(action);
+      for (var i = 0; i < clickers.length; i++) {
+        if (math.equal(i,action)) {
+          clickers[i].style.backgroundColor = "#4CAF50";
+          clickers[i].style.color =  "white";
+        } else {
+          clickers[i].style.backgroundColor = "white";
+          clickers[i].style.color =  "#4CAF50";
+        }
       }
 
     }
