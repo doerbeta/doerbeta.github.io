cf23de46132cfa47dde8f9e4725db1f2b80045dc
Baihan Lin <nature@gatech.edu>
2020-03-01 17:31:45 -0500

    [DBG] change font size

--- js/resume.js
+++ js/resume.js
@@ -26,7 +26,7 @@ function openInfo6() {
 function openInfo(title,image,text) {
   document.getElementById("info").style.width = "60%";
   document.getElementById("info").style.height = "60%";
-  document.getElementById("infotext").innerHTML = '<div class="inforow"><h3 class="infocontent">&nbsp;&nbsp;' + title + '</h3></div><br>' +
+  document.getElementById("infotext").innerHTML = '<div class="inforow"><h2 class="infocontent">&nbsp;&nbsp;' + title + '</h2></div><br>' +
       '<div class="infoimgcolumn"><img src=' + image + ' alt=""  style="width:100%;"></div>' +
       '<div class="infotextcolumn"><p class="infocontent"> ' + text + '</p></div>';
 }
