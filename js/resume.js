f9e76eb2f05e731daf7d053c1f962c55863e4474
Baihan Lin <nature@gatech.edu>
2020-03-01 16:55:14 -0500

    [DBG] fix inline

--- js/resume.js
+++ js/resume.js
@@ -28,7 +28,7 @@ function openInfo(title,image,text) {
   document.getElementById("info").style.height = "67%";
   document.getElementById("infotext").innerHTML = '<div class="row"><h3 class="infocontent">' + title + '</h3></div><br>' +
       '<div class="column"><img src=' + image + ' alt=""  style="width:40%;float:center;"></div>' +
-      '<div class="column"><p class="text-primary infocontent"> ' + text + '</p></div>';
+      '<div class="column"><p class="infocontent"> ' + text + '</p></div>';
 }
 
 function closeInfo() {
