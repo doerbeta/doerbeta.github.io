8292d0ed7b7d1f203549ae65618aeb3bc779941f
Baihan Lin <nature@gatech.edu>
2020-07-25 22:12:42 -0400

    [FEAT] add filter by pub types

--- js/pubs.js
+++ js/pubs.js
@@ -2,14 +2,14 @@ var fd_noabbr = {
   'NS': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:steelblue;" class="fd-filter" id="ns-noab">&nbsp;neuroscience&nbsp;</a>',
   'ML': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:tomato;" class="fd-filter" id="ml-noab">&nbsp;machine learning&nbsp;</a>',
   'CV': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:sandybrown;" class="fd-filter" id="cv-noab">&nbsp;computer vision&nbsp;</a>',
-  'GT': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:mediumpurple;" class="fd-filter" id="gt-noab">&nbsp;geometry & topology&nbsp;</a>',
+  'GT': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:mediumpurple;" class="fd-filter" id="gt-noab">&nbsp;geometry && topology&nbsp;</a>',
   'SB': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:seagreen;" class="fd-filter" id="sb-noab">&nbsp;systems biology&nbsp;</a>',
   'SP': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:plum;" class="fd-filter" id="sp-noab">&nbsp;speech processing&nbsp;</a>',
   'AM': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:skyblue;" class="fd-filter" id="am-noab">&nbsp;applied mathematics&nbsp;</a>',
   'HCI': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:sienna;" class="fd-filter" id="hci-noab">&nbsp;human-computer interaction&nbsp;</a>',
   'IT': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:orange;" class="fd-filter" id="it-noab">&nbsp;information theory&nbsp;</a>',
   'TM': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:salmon;" class="fd-filter" id="tm-noab">&nbsp;translational medicine&nbsp;</a>',
-  'SA': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:lightgrey;" class="fd-filter" id="sa-noab">&nbsp;show all&nbsp;</a>'
+  'SA': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="fd-filter" id="sa-noab">&nbsp;show all&nbsp;</a>'
 };
 
 var fd_abbr = {
@@ -23,14 +23,14 @@ var fd_abbr = {
   'HCI': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:sienna;" class="fd-filter" id="hci-ab">&nbsp;HCI&nbsp;</a>',
   'IT': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:orange;" class="fd-filter" id="it-ab">&nbsp;IT&nbsp;</a>',
   'TM': '<a style="border-radius:4px;padding:1px;color:white;font-size:90%;background-color:salmon;" class="fd-filter" id="tm-ab">&nbsp;TM&nbsp;</a>',
-  'SA': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:lightgrey;" class="fd-filter" id="sa-ab">&nbsp;SA&nbsp;</a>'
+  'SA': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="fd-filter" id="sa-ab">&nbsp;SA&nbsp;</a>'
 };
 
 var fd_noabbr_flipped = {
   'NS': '<a style="border-radius:4px;padding:1px;color:steelblue;font-size:90%;background-color:white;" class="fd-filter" id="ns-noab-fp">&nbsp;neuroscience&nbsp;</a>',
   'ML': '<a style="border-radius:4px;padding:1px;color:tomato;font-size:90%;background-color:white;" class="fd-filter" id="ml-noab-fp">&nbsp;machine learning&nbsp;</a>',
   'CV': '<a style="border-radius:4px;padding:1px;color:sandybrown;font-size:90%;background-color:white;" class="fd-filter" id="cv-noab-fp">&nbsp;computer vision&nbsp;</a>',
-  'GT': '<a style="border-radius:4px;padding:1px;color:mediumpurple;font-size:90%;background-color:white;" class="fd-filter" id="gt-noab-fp">&nbsp;geometry & topology&nbsp;</a>',
+  'GT': '<a style="border-radius:4px;padding:1px;color:mediumpurple;font-size:90%;background-color:white;" class="fd-filter" id="gt-noab-fp">&nbsp;geometry && topology&nbsp;</a>',
   'SB': '<a style="border-radius:4px;padding:1px;color:seagreen;font-size:90%;background-color:white;" class="fd-filter" id="sb-noab-fp">&nbsp;systems biology&nbsp;</a>',
   'SP': '<a style="border-radius:4px;padding:1px;color:plum;font-size:90%;background-color:white;" class="fd-filter" id="sp-noab-fp">&nbsp;speech processing&nbsp;</a>',
   'AM': '<a style="border-radius:4px;padding:1px;color:skyblue;font-size:90%;background-color:white;" class="fd-filter" id="am-noab-fp">&nbsp;applied mathematics&nbsp;</a>',
@@ -54,7 +54,55 @@ var fd_abbr_flipped = {
   'SA': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:white;" class="fd-filter" id="sa-ab-fp">&nbsp;SA&nbsp;</a>'
 };
 
+var tp_abbr = {
+  'J': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="tp-filter" id="j-ab">&nbsp;J&nbsp;</a>',
+  'C': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="tp-filter" id="c-ab">&nbsp;C&nbsp;</a>',
+  'E': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="tp-filter" id="e-ab">&nbsp;E&nbsp;</a>',
+  'P': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="tp-filter" id="p-ab">&nbsp;P&nbsp;</a>',
+  'S': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="tp-filter" id="s-ab">&nbsp;all&nbsp;</a>'
+};
+
+var tp_abbr_flipped = {
+  'J': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:white;" class="tp-filter" id="j-ab-fp">&nbsp;J&nbsp;</a>',
+  'C': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:white;" class="tp-filter" id="c-ab-fp">&nbsp;C&nbsp;</a>',
+  'E': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:white;" class="tp-filter" id="e-ab-fp">&nbsp;E&nbsp;</a>',
+  'P': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:white;" class="tp-filter" id="p-ab-fp">&nbsp;P&nbsp;</a>',
+  'S': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:white;" class="tp-filter" id="s-ab-fp">&nbsp;all&nbsp;</a>'
+};
+
+var tp_noabbr = {
+  'J': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="tp-filter" id="j-noab">&nbsp;peer-reviewed journals&nbsp;</a>',
+  'C': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="tp-filter" id="c-noab">&nbsp;peer-reviewed conference proceedings&nbsp;</a>',
+  'E': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="tp-filter" id="e-noab">&nbsp;peer-reviewed extended abstracts&nbsp;</a>',
+  'P': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="tp-filter" id="p-noab">&nbsp;arXiv preprints / manuscripts&nbsp;</a>',
+  'S': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:gainsboro;" class="tp-filter" id="s-noab">&nbsp;total&nbsp;</a>'
+};
+
+var tp_noabbr_flipped = {
+  'J': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:white;" class="tp-filter" id="j-noab-fp">&nbsp;peer-reviewed journals&nbsp;</a>',
+  'C': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:white;" class="tp-filter" id="c-noab-fp">&nbsp;peer-reviewed conference proceedings&nbsp;</a>',
+  'E': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:white;" class="tp-filter" id="e-noab-fp">&nbsp;peer-reviewed extended abstracts&nbsp;</a>',
+  'P': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:white;" class="tp-filter" id="p-noab-fp">&nbsp;arXiv preprints / manuscripts&nbsp;</a>',
+  'S': '<a style="border-radius:4px;padding:1px;color:black;font-size:90%;background-color:white;" class="tp-filter" id="s-noab-fp">&nbsp;total&nbsp;</a>'
+};
+
 var pubids = {};
+
+function getFields(fd, abbr, st) {
+  if (abbr) {
+    return (st) ? fd_abbr[fd] : fd_abbr_flipped[fd];
+  } else {
+    return (st) ? fd_noabbr[fd] : fd_noabbr_flipped[fd];
+  }
+}
+
+function getTypes(tp, abbr, st) {
+  if (abbr) {
+    return (st) ? tp_abbr[tp] : tp_abbr_flipped[tp];
+  } else {
+    return (st) ? tp_noabbr[tp] : tp_noabbr_flipped[tp];
+  }
+}
 
 var getPubsData = fetch('./data/pubs.xlsx')
   .then((response) => {
@@ -103,7 +151,23 @@ function renderPubs(arr, labs) {
   container.innerHTML = ys['html'] + container.innerHTML;
 
   // add citation summary
-  container.innerHTML = citeSummary(arr, ys['J'], ys['C'], ys['E'], ys['P']) + container.innerHTML;
+  var citations = arr
+    .filter((item) => {
+      return item['cite'];
+    })
+    .map((item) => {
+      return item['cite'];
+    })
+    .reduce((a, b) => {
+      return a + b;
+    })
+    container.innerHTML = `<p class="mb-5 text-primary"> 
+    <br /> <span id="filter_types"></span>
+    <br />
+    <br /> <span id="filter_fields"></span>
+    <br />
+    <br /> Feel free to click on <b> abstract </b> to learn more, or [&nbsp;<a style="color:#268fd6;" 
+    class="collapsibleall" id="expall">expand all abstracts </a>&nbsp;] &nbsp; </p>` + container.innerHTML;
 
   // console.log(container);
 
@@ -145,15 +209,26 @@ function renderPubs(arr, labs) {
   }
 
   // add filters by research fields
-  var filter_container = document.querySelector("#filter_fields");
+  var filter_fields_container = document.querySelector("#filter_fields");
   var fds = ['NS', 'ML', 'CV', 'GT', 'SB', 'SP', 'AM', 'HCI', 'IT', 'TM', 'SA'];
   var fdstate = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
   var fdsindex = { 'ns': 0, 'ml': 1, 'cv': 2, 'gt': 3, 'sb': 4, 'sp': 5, 'am': 6, 'hc': 7, 'it': 8, 'tm': 9, 'sa': 10 };
   var fdcount = fds.map((item) => {
     return arr.filter((x) => { return x['areas'].split('-').includes(item); }).length;
   })
-  // fdstate = fdstate.map((item) => { return 1; });
-  listenFilter(arr, filter_container, fds, fdstate, fdsindex, fdcount);
+  var fdopts = { 'container': filter_fields_container, 'name': fds, 'state': fdstate, 'index': fdsindex, 'count': fdcount };
+
+  // add filters by publication types
+  var filter_type_container = document.querySelector("#filter_types");
+  var tps = ['J', 'C', 'E', 'P', 'S'];
+  var tpstate = [1, 1, 1, 1, 1];
+  var tpsindex = { 'J': 0, 'C': 1, 'E': 2, 'P': 3, 'S': 4 };
+  var tpcount = [ys['J'], ys['C'], ys['E'], ys['P'], ys['J'] + ys['C'] + ys['E'] + ys['P']];
+  var tpopts = { 'container': filter_type_container, 'name': tps, 'state': tpstate, 'index': tpsindex, 'count': tpcount, 'citation': citations };
+  
+  // add listeners to both filters
+  listenFieldFilter(arr, fdopts, tpopts);
+  listenTypeFilter(arr, fdopts, tpopts);
 
   // automatically render lab experiences
   var labs_container = document.querySelector("#labs_container");
@@ -179,7 +254,7 @@ function renderPubs(arr, labs) {
   }).join('');
 
   labs_container.innerHTML = `<p class="mb-5 text-primary"> <table align="left" border=1 frame=void rules=rows class="mb-5 text-primary">
-      <tr style="padding:2px;border-radius:4px 4px 0 0;background-color:lightgrey;">
+      <tr style="padding:2px;border-radius:4px 4px 0 0;background-color:gainsboro;">
           <th> From </th>
           <th> To </th>
           <th> At </th>
@@ -190,13 +265,19 @@ function renderPubs(arr, labs) {
 
 }
 
-function listenFilter(arr, filter_container, fds, fdstate, fdsindex, fdcount) {
+function listenFieldFilter(arr, fdopts, tpopts) {
+
+  filter_container = fdopts['container'] 
+  fds = fdopts['name']
+  fdstate = fdopts['state'] 
+  fdsindex = fdopts['index']
+  fdcount = fdopts['count']
 
   var allids = arr
     .filter((item) => { return item['display']; })
     .map((item) => { return document.getElementById(item['id']); });
 
-  filter_container.innerHTML = `Filter by research areas: ${fds.map((fd, index) => {
+  filter_container.innerHTML = `Filter by <b>research areas</b>: ${fds.map((fd, index) => {
     return (fd == 'SA') ? getFields(fd, 0, fdstate[index]) : getFields(fd, 0, fdstate[index]).replace(/&nbsp;<\/a>/gi, ` (${fdcount[index]})&nbsp;</a>`);
   })
     .map((x, index) => { return { 'html': x, 'count': fdcount[index] }; })
@@ -214,7 +295,9 @@ function listenFilter(arr, filter_container, fds, fdstate, fdsindex, fdcount) {
       if (fdfs[i].id.startsWith("sa")) {
         allids.forEach((item) => { item.style.display = 'block'; });
         fdstate = fdstate.map((item) => { return 1; });
-        // filter_container.innerHTML = `Filter by research areas: ${fds.map((fd, index) => { return getFields(fd, 0, fdstate[index]); }).join('&nbsp;')}`;
+      } else if ( !fdstate[-1] && !fdfs[i].id.startsWith("sa") && fdstate.slice(0,-1).reduce((a, b) => { return a + b; })+1 == fdstate.length - 1  ) {
+        allids.forEach((item) => { item.style.display = 'block'; });
+        fdstate = fdstate.map((item) => { return 1; });
       } else {
         if (fdstate.reduce((a, b) => { return a * b; })) {
           fdstate = fdstate.map((item) => { return 0; });
@@ -224,7 +307,6 @@ function listenFilter(arr, filter_container, fds, fdstate, fdsindex, fdcount) {
           if (fdstate.reduce((a, b) => { return a + b; }) == 0) {
             allids.forEach((item) => { item.style.display = 'block'; });
             fdstate = fdstate.map((item) => { return 1; });
-            // filter_container.innerHTML = `Filter by research areas: ${fds.map((fd, index) => { return getFields(fd, 0, fdstate[index]); }).join('&nbsp;')}`;
           }
         }
         var toshow = fds.filter((item, index) => { return fdstate[index]; });
@@ -232,29 +314,87 @@ function listenFilter(arr, filter_container, fds, fdstate, fdsindex, fdcount) {
         var showids = arr
           .filter((item) => {
             var matched = toshow.filter((x) => { return item['areas'].split('-').includes(x); });
-            return item['display'] & (matched.length > 0);
+            return item['display'] && (matched.length > 0);
           })
           .map((item) => { return document.getElementById(item['id']); });
         var hideids = arr
           .filter((item) => {
             var matched = tohide.filter((x) => { return item['areas'].split('-').includes(x); });
-            return item['display'] & (matched.length > 0);
+            return item['display'] && (matched.length > 0);
           })
           .map((item) => { return document.getElementById(item['id']); });
         hideids.forEach((item) => { item.style.display = 'none'; });
         showids.forEach((item) => { item.style.display = 'block'; });
-        // filter_container.innerHTML = `Filter by research areas: ${fds.map((fd, index) => { return getFields(fd, 0, fdstate[index]); }).join('&nbsp;')}`;
       }
-      listenFilter(arr, filter_container, fds, fdstate, fdsindex, fdcount);
+      fdopts['state'] = fdstate;
+      tpopts['state'] = tpopts['state'].map((item) => { return 1; });;    
+      listenTypeFilter(arr, fdopts, tpopts);
+      listenFieldFilter(arr, fdopts, tpopts);
     });
   }
 }
 
-function getFields(fd, abbr, st) {
-  if (abbr) {
-    return (st) ? fd_abbr[fd] : fd_abbr_flipped[fd];
-  } else {
-    return (st) ? fd_noabbr[fd] : fd_noabbr_flipped[fd];
+function listenTypeFilter(arr, fdopts, tpopts) {
+
+  filter_container = tpopts['container'] 
+  tps = tpopts['name']
+  tpstate = tpopts['state'] 
+  tpsindex = tpopts['index']
+  tpcount = tpopts['count']
+  citations = tpopts['citation']
+
+  var allids = arr
+    .filter((item) => { return item['display']; })
+    .map((item) => { return document.getElementById(item['id']); });
+
+  filter_container.innerHTML = `Filter by <b>publication types</b>: 
+  ${getTypes('S', 0, tpstate[4]).replace(/&nbsp;<\/a>/gi, ` (${tpcount[4]})&nbsp;</a>`)} = ${tps.slice(0,4).map((tp, index) => {
+    return getTypes(tp, 0, tpstate[index]).replace(/&nbsp;<\/a>/gi, ` (${tpcount[index]})&nbsp;</a>`);
+  })
+    .join('&nbsp;+&nbsp;')}, with <a href="https://scholar.google.com/citations?user=H67KJ4cAAAAJ&hl=en">${citations}</a> citations.`;
+
+  var tpfs = [].slice.call(document.getElementsByClassName("tp-filter"));
+  for (let i = 0; i < tpfs.length; i++) {
+    tpfs[i].addEventListener("click", () => {
+      if (tpfs[i].id.startsWith("s-")) {
+        allids.forEach((item) => { item.style.display = 'block'; });
+        tpstate = tpstate.map((item) => { return 1; });
+      } else if ( !tpstate[-1] && !tpfs[i].id.startsWith("s-") && tpstate.slice(0,-1).reduce((a, b) => { return a + b; })+1 == tpstate.length - 1  ) {
+        allids.forEach((item) => { item.style.display = 'block'; });
+        tpstate = tpstate.map((item) => { return 1; });
+      } else {
+        if (tpstate.reduce((a, b) => { return a * b; })) {
+          tpstate = tpstate.map((item) => { return 0; });
+          tpstate[tpsindex[tpfs[i].id.substring(0, 1).toUpperCase()]] = 1;
+        } else {
+          tpstate[tpsindex[tpfs[i].id.substring(0, 1).toUpperCase()]] = !tpstate[tpsindex[tpfs[i].id.substring(0, 1).toUpperCase()]];
+          if (tpstate.reduce((a, b) => { return a + b; }) == 0) {
+            allids.forEach((item) => { item.style.display = 'block'; });
+            tpstate = tpstate.map((item) => { return 1; });
+          }
+        }
+        var toshow = tps.filter((item, index) => { return tpstate[index]; });
+        var tohide = tps.filter((item, index) => { return !tpstate[index]; });
+        var showids = arr
+          .filter((item) => {
+            var matched = toshow.filter((x) => { return item['type'] == x; });
+            return item['display'] && (matched.length > 0);
+          })
+          .map((item) => { return document.getElementById(item['id']); });
+        var hideids = arr
+          .filter((item) => {
+            var matched = tohide.filter((x) => { return item['type'] == x; });
+            return item['display'] && (matched.length > 0);
+          })
+          .map((item) => { return document.getElementById(item['id']); });
+        hideids.forEach((item) => { item.style.display = 'none'; });
+        showids.forEach((item) => { item.style.display = 'block'; });
+      }
+      fdopts['state'] = fdopts['state'].map((item) => { return 1; });;
+      tpopts['state'] = tpstate;    
+      listenFieldFilter(arr, fdopts, tpopts);
+      listenTypeFilter(arr, fdopts, tpopts);
+    });
   }
 }
 
@@ -272,6 +412,8 @@ function citeSummary(arr, J, C, E, P) {
   return `<p class="mb-5 text-primary"> <b>Types: </b> peer-reviewed journals <b>(J)</b>, peer-reviewed conference proceedings <b>(C)</b>, peer-reviewed extended abstracts <b>(E)</b>, arXiv preprints / manuscripts <b>(P)</b>.  &nbsp;
   <br />
   <br /> <b>${(J + C + E + P)}</b> total = <b>${J}</b> J + <b>${C}</b> C + <b>${E}</b> E + <b>${P}</b> P, with <a href="https://scholar.google.com/citations?user=H67KJ4cAAAAJ&hl=en">${citations}</a> citations. &nbsp;
+  <br />
+  <br /> <span id="filter_types"></span>
   <br />
   <br /> <span id="filter_fields"></span>
   <br />
